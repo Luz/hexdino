66bec62878469eefca5c2956ad102b2f34888198
Raphael Nestler <raphael.nestler@gmail.com>
2016-10-05 11:21:57 +0200

    Style: Move comments above the statements

--- src/find.rs
+++ src/find.rs
@@ -7,12 +7,15 @@ impl FindSubset for Vec<u8> {
         if subset.len() > self.len() {
             return None;
         }
-        for a in 0 .. self.len() - subset.len() + 1 { //a in self.iter() { //Search in a for b
-            for b in 0 .. subset.len() { //subset.iter() {
-                if subset[b] != self[a+b] { // if b is not a, skip searching at that position in a
+        //Search in a for b
+        for a in 0 .. self.len() - subset.len() + 1 {
+            for b in 0 .. subset.len() {
+                // if b is not a, skip searching at that position in a
+                if subset[b] != self[a+b] {
                     break; // element does not match
                 }
-                if b == subset.len()-1 { // when all elements of b match in a, return position of a
+                // when all elements of b match in a, return position of a
+                if b == subset.len()-1 {
                     return Some(a);
                 }
             }
@@ -22,55 +25,64 @@ impl FindSubset for Vec<u8> {
 }
 
 #[test]
-fn find_subset_1() { // Partial at start
+// Partial at start
+fn find_subset_1() {
     let buf = vec![1,2,3,4,5];
     let sub = vec![1,2];
     assert_eq!(buf.find_subset(&sub) , Some(0));
 }
 #[test]
-fn find_subset_2() { // Partial at middle
+// Partial at middle
+fn find_subset_2() {
     let buf = vec![1,2,3,4,5];
     let sub = vec![3,4];
     assert_eq!(buf.find_subset(&sub) , Some(2));
 }
 #[test]
-fn find_subset_3() { // Partial at end
+// Partial at end
+fn find_subset_3() {
     let buf = vec![1,2,3,4,5];
     let sub = vec![4,5];
     assert_eq!(buf.find_subset(&sub) , Some(3));
 }
 #[test]
-fn find_subset_4() { // Partial after end
+// Partial after end
+fn find_subset_4() {
     let buf = vec![1,2,3,4,5];
     let sub = vec![5,6];
     assert_eq!(buf.find_subset(&sub) , None);
 }
 #[test]
-fn find_subset_5() { // Partial before start
+// Partial before start
+fn find_subset_5() {
     let buf = vec![1,2,3,4,5];
     let sub = vec![0,1];
     assert_eq!(buf.find_subset(&sub) , None);
 }
 #[test]
-fn find_subset_6() { // short
+// short
+fn find_subset_6() {
     let buf = vec![1,2,3,4,5];
     let sub = vec![2];
     assert_eq!(buf.find_subset(&sub) , Some(1));
 }
 #[test]
-fn find_subset_7() { // too long
+// too long
+fn find_subset_7() {
     let buf = vec![1,2,3,4,5];
     let sub = vec![1,2,3,4,5,6];
     assert_eq!(buf.find_subset(&sub) , None);
 }
 #[test]
-fn find_subset_8() { // full
+// full
+fn find_subset_8() {
     let buf = vec![1,2,3,4,5];
     let sub = vec![1,2,3,4,5];
     assert_eq!(buf.find_subset(&sub) , Some(0));
 }
 #[test]
-fn find_subset_9() { // Swapped
+// Swapped
+fn find_subset_9() {
     let buf = vec![1,2,3,4,5];
     let sub = vec![5,4,3,2,1];
     assert_eq!(buf.find_subset(&sub) , None);
