be202a9f701b602477b1e47b3b009e92764dccfe
Luz <luz666@daum.net>
2021-03-14 22:41:11 +0100

    Save condition from rustfmt

--- src/find.rs
+++ src/find.rs
@@ -24,12 +24,14 @@ impl FindOptSubset for Vec<u8> {
                 // or first nibble matches when second nibble is wildcard,
                 // or second nibble matches when first nibble is wildcard.
                 // or two wildcards at that position.
+                #![rustfmt::skip]
                 if !(
                     ((subset[2*b  ] == self[a + b]>>4) && (subset[2*b+1] == self[a + b]%16)) ||
                     ((subset[2*b  ] == self[a + b]>>4) && (subset[2*b+1] >= 0x10)) ||
                     ((subset[2*b+1] == self[a + b]%16) && (subset[2*b  ] >= 0x10)) ||
                     ((subset[2*b+1] >= 0x10) && (subset[2*b  ] >= 0x10))
-                   ) {
+                   )
+                {
                     break; // element does not match
                 }
                 // when all elements of b match in a, return position of a
